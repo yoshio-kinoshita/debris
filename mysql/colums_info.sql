a153325b1a308136c9dfd615a519332d09ecffe2
yoshio kinoshita <g-ykinoshita@gmo-pg.com>
2013-02-25 11:27:29 +0900

    Signed-off-by: yoshio kinoshita <g-ykinoshita@gmo-pg.com>

--- mysql/colums_info.sql
+++ mysql/colums_info.sql
@@ -1,13 +1,14 @@
 SELECT
-	-- *
-	TABLE_NAME as 'テーブル名'
- , COLUMN_NAME as 'カラム名'
- ,	column_default as 'デフォルト値'
- , CHARACTER_MAXIMUM_LENGTH as '最大レングス'
- , CHARACTER_OCTET_LENGTH as '最大バイト数'
- , COLUMN_TYPE as 'カラムタイプ'
- , column_comment as 'コメント'
-FROM
+ 	TABLE_NAME as 'テーブル名'
+  , COLUMN_NAME as 'カラム名'
+  ,	column_default as 'デフォルト値'
+  , CHARACTER_MAXIMUM_LENGTH as '最大レングス'
+  , CHARACTER_OCTET_LENGTH as '最大バイト数'
+  , COLUMN_TYPE as 'カラムタイプ'
+  , column_comment as 'コメント'
+  , character_set_name as 'キャラクタセット'
+ , column_key as 'キータイプ'
+ FROM
 	information_schema.`COLUMNS`
 where
 	table_schema != 'information_schema'
